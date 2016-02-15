08a78ef10091c01612f5058b25f8fdf20fc04ec6
Denis Molony <dmolony@users.noreply.github.com>
2016-02-16 07:49:14 +1100

    Update dm3270.rb

--- dm3270.rb
+++ dm3270.rb
@@ -1,8 +1,8 @@
 class dm3270 < Formula
   desc "tn3270 Terminal Emulator"
   homepage "https://github.com/dmolony/dm3270
-  url "https://github.com/dmolony/dm3270download/v0.5/dm3270.jar"
-  version "0.5"
+  url "https://github.com/dmolony/dm3270/download/v0.5-beta-36/dm3270.jar"
+  version "0.5 beta 36"
   sha256 "xxx"
 
   depends_on :java => "1.8+"
