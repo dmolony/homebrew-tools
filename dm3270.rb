4742e8b276520ad97984e6d4a3e1ac683f5350b9
Denis Molony <dmolony@users.noreply.github.com>
2016-02-16 07:07:05 +1100

    Create dm3270.rb

---
+++ dm3270.rb
@@ -0,0 +1,18 @@
+class dm3270 < Formula
+  desc "tn3270 Terminal Emulator"
+  homepage "https://github.com/dmolony/dm3270
+  url "https://github.com/dmolony/dm3270download/v0.5/dm3270.jar"
+  version "0.5"
+  sha256 "xxx"
+
+  depends_on :java => "1.8+"
+
+  def install
+    libexec.install "dm3270.jar"
+    bin.write_jar_script libexec/"dm3270.jar", "dm3270"
+  end
+
+  test do
+    system "false"
+  end
+end
