3cdf070204fdb712a479a08ae4df197536e40e5d
Denis Molony <dmolony@users.noreply.github.com>
2016-07-17 12:19:55 +1000

    Update diskbrowser.rb

--- diskbrowser.rb
+++ diskbrowser.rb
@@ -1,8 +1,8 @@
 class Diskbrowser < Formula
   desc "Apple II Disk Browser"
   homepage "https://github.com/dmolony/DiskBrowser"
-  url "https://github.com/dmolony/DiskBrowser/releases/download/v1.0.10/DiskBrowser.jar"
-  version "1.0.10"
+  url "https://github.com/dmolony/DiskBrowser/releases/download/v1.0.11/DiskBrowser.jar"
+  version "1.0.11"
   sha256 "2a4c65a3557863724237f29e282bf65aaf739ebf674ca0d290c400b657fdd413"
 
   depends_on :java => "1.6+"
